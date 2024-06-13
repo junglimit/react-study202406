e351a389d78dd395a00d2b762b2f72d72c10dee4
junglimit <jaehan_jjh@naver.com>
2024-06-13 10:41:16 +0900

    props 사용법

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ const App = () => {
     const hello = '안녕안녕'
     return (
         <>
-            <ExpenseItem />
-            <ExpenseItem />
+            <ExpenseItem title={'치킨먹음'} price={30000} date={new Date(2024, 6, 3)} />
+            <ExpenseItem title='족발먹음' price={40000} date={new Date(2024, 6, 7)} />
+            <ExpenseItem title='헬스장등록' price={300000} date={new Date(2024, 6, 12)} />
         </>
     );
 }
