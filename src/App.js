1d61bbf97693f309814503436c6a5468b243c006
junglimit <jaehan_jjh@naver.com>
2024-06-13 10:49:06 +0900

    props 사용법 / JSON 가정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import './App.css';
 import ExpenseItem from "./components/expenses/ExpenseItem";
@@ -6,12 +5,44 @@ import ExpenseItem from "./components/expenses/ExpenseItem";
 
 const App = () => {
 
-    const hello = '안녕안녕'
+    // 서버에서 지출항목 JSON 배열을 응답받은 가정
+    const expenses = [
+        {
+            title: '치킨먹음',
+            price: 30000,
+            date: new Date(2024,6,3)
+        },
+        {
+            title: '족발먹음',
+            price: 40000,
+            date: new Date(2024,6,7)
+        },
+        {
+            title: '헬스장등록',
+            price: 300000,
+            date: new Date(2024,6,12)
+        }
+
+    ];
+
+
     return (
         <>
-            <ExpenseItem title={'치킨먹음'} price={30000} date={new Date(2024, 6, 3)} />
-            <ExpenseItem title='족발먹음' price={40000} date={new Date(2024, 6, 7)} />
-            <ExpenseItem title='헬스장등록' price={300000} date={new Date(2024, 6, 12)} />
+            <ExpenseItem
+                title={expenses[0].title}
+                price={expenses[0].price}
+                date={expenses[0].date}
+            />
+            <ExpenseItem
+                title={expenses[1].title}
+                price={expenses[1].price}
+                date={expenses[1].date}
+            />
+            <ExpenseItem
+                title={expenses[2].title}
+                price={expenses[2].price}
+                date={expenses[2].date}
+            />
         </>
     );
 }
