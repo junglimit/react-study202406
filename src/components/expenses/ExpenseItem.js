6bcb1016b0f98e9e2388f6b6eed3d523a5e08772
junglimit <jaehan_jjh@naver.com>
2024-06-13 12:25:47 +0900

    코드 정리

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -5,29 +5,8 @@ import Card from "../UI/Card";
 
 const ExpenseItem = ({title, price: exPrice, date}) => {
 
-    // console.log('props: ', aaa)
-
-    // 변수 선언
-    // const expenseDate = date;
-    // const expenseTitle = title;
-    // const expensePrice = exPrice;
-
 
     // 함수 선언
-
-    // 1자리 숫자를 2자리수로 변환하는 함수
-    const make2digit = (text) => {
-        return text.toString().padStart(2, '0');
-    };
-
-    const makeFormattedDate = () => {
-
-        const year = date.getFullYear();
-        const month = date.getMonth();
-        const day = date.getDate();
-
-        return `${year}년 ${make2digit(month)}월 ${make2digit(day)}일`;
-    };
 
     // 원화 표기법으로 변환
     const formattedPrice = new Intl.NumberFormat('ko-KR').format(exPrice)
