2dbbae4c0742b5f5892eb9cbd3a8f651ca0e03da
junglimit <jaehan_jjh@naver.com>
2024-06-13 09:52:42 +0900

    react 컴포넌트

---
+++ src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react'; // 생략가능 ( 구버전은 X )
+
+const Greet = () =>{
+
+    return(
+        <ul>
+            <li>사과</li>
+            <li>딸기</li>
+            <li>오렌지</li>
+        </ul>
+    );
+}
+
+export default Greet;
---
+++ src/components/Hello.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Hello extends React.Component {
+
+    render() {
+
+
+        return(
+            <>
+            <h1>냐냐나나</h1>
+            <h2>냠냠냠꺽</h2>
+            </>
+        );
+    }
+}
+
+export default Hello;
