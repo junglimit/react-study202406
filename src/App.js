7597464277e0d7f737a6b52d9cb7d539bacd928c
junglimit <jaehan_jjh@naver.com>
2024-06-13 11:19:51 +0900

    컴포넌트 세분화

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import ExpenseItem from "./components/expenses/ExpenseItem";
+import ExpenseList from "./components/expenses/ExpenseList";
 
 
 const App = () => {
@@ -28,21 +28,7 @@ const App = () => {
 
     return (
         <>
-            <ExpenseItem
-                title={expenses[0].title}
-                price={expenses[0].price}
-                date={expenses[0].date}
-            />
-            <ExpenseItem
-                title={expenses[1].title}
-                price={expenses[1].price}
-                date={expenses[1].date}
-            />
-            <ExpenseItem
-                title={expenses[2].title}
-                price={expenses[2].price}
-                date={expenses[2].date}
-            />
+            <ExpenseList expenses={expenses} />
         </>
     );
 }
