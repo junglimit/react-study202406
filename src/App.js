c171bbb2562229cd687f511d1d4c0297ed94fff2
junglimit <jaehan_jjh@naver.com>
2024-06-13 10:28:30 +0900

    동적 데이터 렌더링

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 
-import './App.css';
 import React from "react";
-import Hello from "./components/Hello";
-import Bye from "./components/Bye";
-import Greet from "./components/Greet";
-
-function App() {
+import './App.css';
+import ExpenseItem from "./components/expenses/ExpenseItem";
 
 
-    // jsx 문법
-    // const $h2 = React.createElement('h2', null, '방가방가햄토리');
-    const $h2 = <h2>방가봉가햄톨이</h2>;
+const App = () => {
 
-    // jsx 규칙
-    // 1. return 안에 있는 태그는 반드시 하나의 태그로 묶여야함. <React.Fragment>로 묶기
-    // 2. 빈 태그(닫는 태그 없는)는 반드시 /> 로 마감
-    // 3. 태그의 class 속성은 자바스크립트 키워드 class 와 겹쳐서 className 으로 표기
-    // 4. 의미없는 부모는 <React.Fragment> 로 감싸면 됨 , <> 로 표현가능
-    // 5. 변수값이나 함수를 출력할 때는 {} 로 감싸면 됨
     const hello = '안녕안녕'
     return (
         <>
-            <Hello />
-            <Bye />
-            <Greet/>
+            <ExpenseItem />
+            <ExpenseItem />
         </>
     );
 }
