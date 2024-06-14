b71b7e9172a3bff3ed20473e9513bca7544676c9
junglimit <jaehan_jjh@naver.com>
2024-06-14 11:45:35 +0900

    목표설정 앱

---
+++ src/components/CourseGoals/CourseItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CourseItem.css';
+
+const CourseItem = ({item}) => {
+    return (
+        <li className='goal-item'>
+            {item.text}
+        </li>
+    );
+};
+
+export default CourseItem;
