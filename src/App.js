1a0b11fca28ac52b3aac28840f3521fe12e3ad07
junglimit <jaehan_jjh@naver.com>
2024-06-18 09:49:09 +0900

    로그인 시 발생하는 side effect / 무한루프

--- src/App.js
+++ src/App.js
@@ -7,12 +7,34 @@ import Login from "./components/SideEffect/Login";
 
 const App = () => {
 
+    // 현재 로그인 상태를 체크하는 변수
+    const[isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // localStorage 에서 login-flag 를 꺼냄
+    const storedLoginFlag = localStorage.getItem("login-flag");
+    // 로그인 검사를 초기에 한번만 수행
+    if(storedLoginFlag === '1'){
+        // 상태변수가 setter 로 변경되면
+        // 리액트는 변경감지 후 바로 리렌더링을 수행함
+        setIsLoggedIn(true);
+    }
+
+    // 서버통신은 중앙집중 관리가 중요함
+    const loginHandler = (email, password) => {
+
+
+
+        // 로그인의 증거로 클라이언트에 1이라는 숫자를 기록
+        localStorage.setItem('login-flag', '1');
+        setIsLoggedIn(true);
+    };
+
     return (
         <>
             <MainHeader />
             <main>
-                {/*<Home />*/}
-                <Login />
+                {isLoggedIn && <Home />}
+                {!isLoggedIn &&<Login onLogin={loginHandler} />}
             </main>
 
         </>
