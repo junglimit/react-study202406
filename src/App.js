44adb93333561de1a4ef295b9ec32843f01b8e37
junglimit <jaehan_jjh@naver.com>
2024-06-19 09:33:38 +0900

    Context 기초

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import MainHeader from "./components/SideEffect/MainHeader";
 import Home from "./components/SideEffect/Home";
 import Login from "./components/SideEffect/Login";
 
+import AuthContext from "./store/auth-context";
+
 
 const App = () => {
 
@@ -39,20 +41,22 @@ const App = () => {
         setIsLoggedIn(true);
     };
 
+    // 로그아웃 실행 함수
     const logoutHandler = () => {
         localStorage.removeItem('login-flag');
         setIsLoggedIn(false);
     }
 
     return (
-        <>
+        <AuthContext.Provider value={{
+            isLoggedIn: isLoggedIn
+        }}>
             <MainHeader onLogout={logoutHandler} />
             <main>
                 {isLoggedIn && <Home/>}
                 {!isLoggedIn && <Login onLogin={loginHandler}/>}
             </main>
-
-        </>
+        </AuthContext.Provider>
     );
 };
 
