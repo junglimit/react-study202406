1564cb3ee09066f7697c4ae71cc8a7d82a4ae41f
junglimit <jaehan_jjh@naver.com>
2024-06-20 12:10:57 +0900

    forwardRef 사용하기

--- src/components/Ref/ResultModal.js
+++ src/components/Ref/ResultModal.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
-const ResultModal = ({result, targetTime, ref}) => {
-
+const ResultModalComponent = ({result, targetTime}, ref) => {
 
     return (
-        <dialog ref={ref} className="result-modal" >
+        <dialog ref={ref} className="result-modal">
             <h2>Your {result}!</h2>
             <p>The target time was <strong>{targetTime} seconds.</strong></p>
             <p>You stopped the timer with <strong>X seconds left.</strong></p>
@@ -14,5 +13,9 @@ const ResultModal = ({result, targetTime, ref}) => {
         </dialog>
     );
 };
+
+// forwardRef는 콜백으로 전달된 컴포넌트에 첫번째 파라미터로 props를 전달하고
+// 두번째 파라미터에는 부모에게 받은 ref를 전달한다.
+const ResultModal = forwardRef(ResultModalComponent);
 
 export default ResultModal;
