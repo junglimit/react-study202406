3a024a6124874a772ed884089413853e2cf12351
junglimit <jaehan_jjh@naver.com>
2024-06-24 10:42:37 +0900

    장바구니 + 버튼 처리

--- src/components/Food/Cart/CartItem.js
+++ src/components/Food/Cart/CartItem.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import styles from './CartItem.module.scss';
 
+import CartContext from "../../../store/cart-context";
+
 const CartItem = ({ cart }) => {
+
+    const {addItem} = useContext(CartContext);
 
     const { name, price, amount } = cart;
 
@@ -15,6 +19,14 @@ const CartItem = ({ cart }) => {
     } = styles;
     const formatPrice = new Intl.NumberFormat('ko-KR').format(price);
 
+    const cartAddHandler = e => {
+        const item = {
+             ...cart,
+            amount: 1,
+        }
+        addItem(item);
+    }
+
     return (
         <li className={cartItem}>
             <div>
@@ -26,7 +38,7 @@ const CartItem = ({ cart }) => {
             </div>
             <div className={actions}>
                 <button>−</button>
-                <button>+</button>
+                <button onClick={cartAddHandler}>+</button>
             </div>
         </li>
     );
