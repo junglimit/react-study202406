5bf530661c7aba0b497013fea296ac6126f28533
junglimit <jaehan_jjh@naver.com>
2024-07-03 09:31:10 +0900

    router : 복습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import Home from './components/RouteExample/pages/Home';
-import Products from './components/RouteExample/pages/Products';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './components/RouteExample/layout/RootLayout';
 import ErrorPage from './components/RouteExample/pages/ErrorPage';
-import ProductDetail from './components/RouteExample/pages/ProductDetail';
+import Events from './components/RouteExample/pages/Events';
+import EventDetail from './components/RouteExample/pages/EventDetail';
 
 // 라우터 설정
 const router = createBrowserRouter([
-
     {
         path: '/',
         element: <RootLayout />,
         errorElement: <ErrorPage />,
         children: [
             { index: true, element: <Home /> },
-            { path: 'products', element: <Products /> },
-            { path: 'products/:prodId/page/:pageNo', element: <ProductDetail /> },
+            { path: 'events', element: <Events /> },
+            { path: 'events/:eventId', element: <EventDetail /> },
         ]
     },
-
 ]);
 
 const App = () => {
