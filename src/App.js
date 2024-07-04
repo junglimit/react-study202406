5c6481255c6206fd7916aee729d9251bad2936da
junglimit <jaehan_jjh@naver.com>
2024-07-04 11:10:12 +0900

    EventForm 제어

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Events, { loader as eventListLoader } from './components/RouteExample/pag
 import EventDetail, { loader as eventDetailLoader } from './components/RouteExample/pages/EventDetail';
 import EventLayout from './components/RouteExample/layout/EventLayout';
 import NewEvent from './components/RouteExample/pages/NewEvent';
-import EditPage from "./components/RouteExample/pages/EditPage";
+import EditPage from './components/RouteExample/pages/EditPage';
 
 // 라우터 설정
 const router = createBrowserRouter([
@@ -29,10 +29,13 @@ const router = createBrowserRouter([
                     {
                         path: ':eventId',
                         loader: eventDetailLoader,
-                        id: 'event-detail',
+                        // element: <EventDetail />,
+                        // loader가 children에게 직접적으로 연결되지 않아
+                        // EventDetail에서 loader를 사용하지 못하고 있음.
+                        id: 'event-detail', // loader에게 ID 부여
                         children: [
                             { index: true, element: <EventDetail /> },
-                            { path: 'edit', element: <EditPage /> }
+                            { path: 'edit', element: <EditPage /> },
                         ]
                     },
                     { path: 'new', element: <NewEvent /> },
